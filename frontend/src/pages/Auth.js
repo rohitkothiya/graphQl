efd2e967af58fd8a89e46cf56172c5ee4bc158a9
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-02-24 09:35:50 +0530

    github

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -95,7 +95,7 @@ class AuthPage extends Component {
           <input type="password" id="password" ref={this.passwordEl} />
         </div>
         <div className="form-actions">
-          <button type="submit">Submit</button>
+          <button type="submit">Continue</button>
           <button type="button" onClick={this.switchModeHandler}>
             Switch to {this.state.isLogin ? 'Signup' : 'Login'}
           </button>
