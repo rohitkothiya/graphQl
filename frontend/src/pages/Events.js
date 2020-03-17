91602d1470419f9cdcd74f74158f5e9c4a0c5ab8
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-03-17 18:59:16 +0530

    git

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -226,7 +226,7 @@ class EventsPage extends Component {
                 <input type="text" id="title" ref={this.titleElRef} />
               </div>
               <div className="form-control">
-                <label htmlFor="price">Price</label>
+                <label htmlFor="price">price</label>
                 <input type="number" id="price" ref={this.priceElRef} />
               </div>
               <div className="form-control">
