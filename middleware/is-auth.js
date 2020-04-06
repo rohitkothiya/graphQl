62b9caa0c6540e891a971bc5d465d52a0f6e40fd
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-04-06 14:34:18 +0530

    git

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
       const authHeader = req.get('Authorization')
-      console.log("h", authHeader)
+      console.log("hauthheader", authHeader)
       if (!authHeader) {
             req.isAuth = false
 
