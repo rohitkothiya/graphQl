04539294d80ada8af861082228bcfdd0253a7654
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-04-22 10:35:49 +0530

    git

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
       const token = authHeader.split(' ')[1];//Authorizatiuon
 
       if (!token || token === "") {
-            console.log("elsze")
+            console.log("elsz")
             req.isAuth = false
             next()
       }
