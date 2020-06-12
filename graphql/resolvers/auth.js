f598917edf3ccdc51c06cad99dcbdf87065590ed
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-06-12 19:01:23 +0530

    git

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -36,7 +36,7 @@ module.exports = {
     }
     const token = jwt.sign(
       { userId: user.id, email: user.email },
-      "somesupsaserkesaszy",
+      "somesuspsaserkesaszy",
       {
         expiresIn: "1h"
       }
