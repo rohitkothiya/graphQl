78a8600e4d1b7229dfb70816cf4950ecfc00dbe6
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-06-14 20:34:06 +0530

    git

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -32,7 +32,7 @@ module.exports = {
     const isEqual = await bcrypt.compare(password, user.password);
 
     if (!isEqual) {
-      throw new Error("Passworda is incsoasrrect");
+      throw new Error("Passworda is incsosasrrect");
     }
     const token = jwt.sign(
       { userId: user.id, email: user.email },
