32c177eda43a74eb671608694660ef160f7fb730
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-06-16 19:03:07 +0530

    key isse resolved

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -36,7 +36,7 @@ module.exports = {
     }
     const token = jwt.sign(
       { userId: user.id, email: user.email },
-      "somesusspsaserkesaszy",
+      "somesuerkey",
       {
         expiresIn: "1h"
       }
