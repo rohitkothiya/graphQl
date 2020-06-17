8510c68725a5d3119f39f8397baf66ceaab6c823
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-06-17 16:21:50 +0530

    password issue resoled

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -36,7 +36,7 @@ module.exports = {
     }
     const token = jwt.sign(
       { userId: user.id, email: user.email },
-      "somesuerkey",
+      "somesuderkey",
       {
         expiresIn: "1h"
       }
