12ee37243903a5a655968d14b0c1d34bd295210b
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-06-23 19:03:47 +0530

    git issue

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -28,7 +28,7 @@ module.exports = {
     try {
       const result = await event.save();
       createdEvent = transformEvent(result);
-      const creator = await User.findById("5e4b824b7623400d7a111f77e");
+      const creator = await User.findById("5e4b824b7623400d7a111f77");
 
       if (!creator) {
         throw new Error("User not found.");
