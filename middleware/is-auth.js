c92962f530008e5a854b34aa714c98b034a71b67
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-07-03 22:54:34 +0530

    git

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
             decodedToken = jwt.verify(token, 'someanysuperscretkey')
             console.log("decoddeTsoksen", decodedToken)
       } catch (err) {
-            console.log("errseszs")
+            console.log("errsesszs")
             req.isAuth = false
             return next()
       }
